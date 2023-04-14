e4342ee05fb48a3f0ab8611b2fc48445aac416b5
Denis Yuliyanov <deniskaradaliev@gmail.com>
2023-04-14 11:25:08 +0200

    Stack Navigation

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-import { View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Campobase from './componentes/CampobaseComponent';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <View>
-        <Campobase />
-        <StatusBar style='auto' />
-      </View>
-    </SafeAreaProvider>
+  return(
+  <SafeAreaProvider>
+    <View style={styles.container}>
+      <Campobase/>
+      <StatusBar style="auto" />
+    </View>
+  </SafeAreaProvider>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#afa',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
